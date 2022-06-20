81eddf9e0d9d3d21440a6c644ee983df935ec1db
dayanersilva <dayane.rodrigues_silva@hotmail.com>
2022-06-20 14:36:56 -0300

    Iniciando a CLI

---
+++ cli.js
@@ -0,0 +1,3 @@
+const mdLinks = require('./index');
+const path = process.argv;
+console.log(mdLinks(path[2]));
--- index.js
+++ index.js
@@ -1,3 +1,49 @@
-module.exports = () => {
-  // ...
-};
+const fs = require('fs');
+
+const mdLinks = (arquivo) => {
+  const linkRegex = /\[([^\]]*)\]\(https?:\/\/[^$#\s].[^\s]*\)/gm;
+  return new Promise((resolve, reject) => {
+    fs.readFile(arquivo, 'utf8', (erro, texto) => {
+      if (erro) {
+        //if(path.extname(arquivo) !== ".md")
+        //reject('Formato Inválido')
+        reject(error(erro))
+      } else {
+        const linkExtraido = texto.match(linkRegex)
+        linkExtraido.map((link) => {
+          const arrLink = link.replace('[', '').split('](');
+          const strObject = {
+            text: arrLink[0],
+            link: arrLink[1],
+            file: arquivo,
+          };
+          console.log(strObject);
+        })
+
+        resolve(console.log())
+      }
+    })
+  })
+}
+
+function error(erro) {
+  throw new Error(erro.code, "Não há arquivo no caminho")
+}
+
+module.exports = mdLinks
+
+// function error(erro) {
+//   throw new Error(erro.code, "Não há arquivo no caminho")
+// }
+
+// function pegarArquivo(caminhoDoArquivo) {
+//   fs.readFile(caminhoDoArquivo, 'utf8', (erro, texto) => {
+//     if (erro) {
+//       error(erro)
+//     }
+//     console.log(texto)
+//   })
+// }
+
+// pegarArquivo('./README.md');
+''
